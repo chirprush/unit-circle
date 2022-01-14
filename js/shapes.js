9426a97e36fe634b8742e2e07e93d9cbf28eba71
chirprush <chirpyboat73@gmail.com>
2022-01-13 23:14:11 -0500

    Added a right triangle shape

--- js/shapes.js
+++ js/shapes.js
@@ -52,6 +52,41 @@ class Point {
 	}
 }
 
+class RightTriangle {
+	constructor(pos, length, color) {
+		this.pos = pos;
+		this.length = length;
+		this.color = color || new Color(0xffffffff);
+	}
+
+	render(window) {
+		let pos = this.pos();
+		let length = this.length();
+		let right_angle_square_length = 10; // Math.sqrt(Math.pow(length.x, 2) + Math.pow(length.y, 2)) / 5;
+		window.ctx.strokeStyle = this.color.toString();
+		window.ctx.lineWidth = 3;
+		window.ctx.moveTo(pos.x, pos.y);
+		window.ctx.lineTo(pos.x + length.x, pos.y);
+		window.ctx.lineTo(pos.x + length.x, pos.y - length.y);
+		window.ctx.lineTo(pos.x, pos.y);
+		window.ctx.lineWidth = 2;
+		let length_x_mult = 1;
+		let length_y_mult = 1;
+		if (length.x < 0) {
+			length_x_mult = 0;
+		}
+		if (length.y < 0) {
+			length_y_mult = 0;
+		}
+		let a = this.color.a;
+		this.color.a = 0.7;
+		window.ctx.strokeStyle = this.color.toString();
+		this.color.a = a;
+		window.ctx.rect(pos.x + length.x + length_x_mult * -right_angle_square_length, pos.y + length_y_mult * -right_angle_square_length, right_angle_square_length, right_angle_square_length);
+		window.ctx.stroke();
+	}
+}
+
 class Text {
 	constructor(pos, text, font, color) {
 		this.pos = pos;
@@ -69,4 +104,4 @@ class Text {
 	}
 }
 
-export { Circle, Line, Point, Text };
+export { Circle, Line, Point, RightTriangle, Text };
--- js/visual.js
+++ js/visual.js
@@ -1,47 +1,17 @@
 import { Canvas } from "./draw.js";
 import { Vec } from "./vec.js";
 import { Color } from "./color.js";
-import { Circle, Line, Point, Text } from "./shapes.js";
+import { Circle, Line, Point, RightTriangle, Text } from "./shapes.js";
 
 const bg = new Color(0x03070fff);
 const circle_canvas = new Canvas("circle-canvas", true);
 
-const point = new Point(() => new Vec(
-	Math.floor(circle_canvas.width / 2),
-	Math.floor(circle_canvas.height / 2)
-));
-
-let angle = 0;
-const radius = 100;
-
-const line = new Line(point.pos, () =>
-	new Vec(radius, angle / 180 * Math.PI).toCartesian().add(point.pos()),
-	new Color(0xffffff7f)
-);
-
-const mouse_line = new Line(point.pos, () => {
-	let angle = circle_canvas.mousePos.sub(point.pos()).toPolar().y;
-	return new Vec(radius * Math.cos(angle), radius * Math.sin(angle)).add(point.pos());
-});
-
-const end_point = new Point(line.end);
-const mouse_end_point = new Point(mouse_line.end);
-
-const circle = new Circle(point.pos, () => radius);
-
-const text = new Text(() => new Vec(point.pos().x + 10, point.pos().y - 10), () => "θ", '15px "Roboto Mono"');
+const right_triangle = new RightTriangle(() => new Vec(300, 300), () => new Vec(-100, -100));
 
 const frame = () => {
 	requestAnimationFrame(frame);
 	circle_canvas.fill(bg);
-	point.render(circle_canvas);
-	line.render(circle_canvas);
-	mouse_line.render(circle_canvas);
-	mouse_end_point.render(circle_canvas);
-	end_point.render(circle_canvas);
-	circle.render(circle_canvas);
-	text.render(circle_canvas);
-	angle = (angle - 1) % 360;
+	right_triangle.render(circle_canvas);
 }
 
 window.onload = frame;
